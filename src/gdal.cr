133caf036d7de69181a26e63d9dd1fe0245c645d
Ricardo E. Gonzalez <ricardog@ricardog.com>
2020-03-09 10:01:40 +0000

    Add a brief description of the packaged to the top of gdal.cr

--- src/gdal.cr
+++ src/gdal.cr
@@ -1,4 +1,22 @@
-# TODO: Write documentation for `Gdal.cr`
+# ## A very thin wrapper for the GDAL library
+#
+# This library provides a very thin wrapper for GDAL.  It does not
+# provide any abstraction on top of the C API so you end up writing C
+# code in crystal, i.e. you need to do all the memory management (see
+# the GDAL API documentation for more info).
+#
+# The GDAL library error handling mechanism by default logs stuff to
+# stdout.  You can silence these messages by setting
+# GDAL::CPL.silence_failures to true (see sample.cr).  I also provide a
+# couple of functions to wrap calls to GDAL that turn errors into
+# exceptions.
+#
+# See sample.cr for a couple of use cases.
+#
+# TODO: The Libs are based on GDAL 2.2.3.  I plan to add support for
+# versions 2.0 through 3.0+.  Not likely to support versions earlier
+# than 2.0, i.e. 1.11.
+#
 require "semantic_version"
 
 require "./gdal/*"
